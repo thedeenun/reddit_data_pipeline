f4bad90a6ac1ac0388f8ecd4d86f165890e53bbd
Adeenun Abdullee <adeenunabdullee@outlook.com>
2024-10-20 16:21:35 +0700

    add extract reddit api code

---
+++ airflow/code/extract_reddit_api.py
@@ -0,0 +1,54 @@
+import requests
+import pandas as pd
+import os
+from dotenv import load_dotenv, dotenv_values 
+
+load_dotenv() 
+
+def connect_reddit_auth():
+    auth = requests.auth.HTTPBasicAuth(os.getenv('CLIENT_ID'), os.getenv('SECRET_KEY'))
+    data = {'grant_type': 'password',
+            'username': os.getenv('REDDIT_USERNAME'),
+            'password': os.getenv('REDDIT_PASSWORD')}
+
+    headers={'User-Agent': 'MyBot/0.0.1'}
+
+    # send our request for an OAuth token
+    res = requests.post('https://www.reddit.com/api/v1/access_token',
+                        auth=auth, data=data, headers={'User-Agent': 'MyBot/0.0.1'})
+    TOKEN = res.json()['access_token']
+
+    # add authorization to our headers dictionary
+    headers = {**headers, **{'Authorization': f"bearer {TOKEN}"}}
+
+    return headers
+
+def extract_data(headers):
+    keyword = 'Garmin'
+    res = requests.get(f"https://oauth.reddit.com/r/{keyword}/hot",
+                    headers=headers)
+    return res.json
+
+def transform_data(data):
+    POST_FIELDS = (
+        "id",
+        "title",
+        "score",
+        "num_comments",
+        "author",
+        "selftext",
+        "created_utc",
+        "url",
+        "upvote_ratio",
+        "over_18",
+        "edited",
+        "spoiler",
+        "stickied",
+    )
+
+    df = pd.DataFrame()  # Initialize an empty DataFrame
+    for post in data.json()['data']['children']:
+        df_temp = pd.DataFrame({field: [post['data'][field]] for field in POST_FIELDS})  # Build a single-row DataFrame
+        df = pd.concat([df, df_temp], ignore_index=True)  # Concatenate with the main DataFrame
+
+    return df
